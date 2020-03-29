2ef8b46173f35352879b395897a7591b4093fe29
fengxianghu <fengxianghu026@gmail.com>
2020-03-30 05:41:01 +0800

    字面量常量的视频

---
+++ ConstDemo.java
@@ -0,0 +1,12 @@
+public class ConstDemo{
+	
+public static void main(String[] args){
+	
+	System.out.println("XXX");
+	System.out.println(123);
+	System.out.println(250);
+	System.out.println(true);
+	System.out.println(false);
+}
+
+}
