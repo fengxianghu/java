9e4f398d27a404672cec7d018047b7e393b9aeb5
fengxianghu <fengxianghu026@gmail.com>
2020-04-08 05:14:33 +0800

    Switch只能用byte，short,int,char

---
+++ SwitchText.java
@@ -0,0 +1,15 @@
+class SwitchText{
+	public static void main(String[] args) {
+	
+	int weekday = 10;
+
+	switch(weekday){
+	case 1: System.out.println("哈哈1");break; 
+
+	case 2: System.out.println("哈哈2");break;
+
+	default: System.out.println("哈哈2");	
+	}
+
+}
+}
